c5ad0d393c781be8f071d08befa96e13d6079653
Dima_Garanin <garanin203@bk.ru>
2020-02-13 22:35:08 +0400

    №4 вид карты

--- big_map.py
+++ big_map.py
@@ -4,6 +4,45 @@ import pygame
 import requests
 
 z = 1
+l = "map"
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class chouse_bar:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.images = [load_image("shema.png"), load_image("sputnik.png"), load_image("gibrid.png")]
+        self.image = self.images[0]
+
+    def check(self, coor):
+        if coor[1] > 0 and coor[1] < 30:
+            if coor[0] < 66:
+                self.image = self.images[0]
+                return "map"
+            elif coor[0] < 133:
+                self.image = self.images[1]
+                return "sat"
+            elif coor[0] < 200:
+                self.image = self.images[2]
+                return "sat,skl"
+        return False
+
+    def draw(self):
+        screen.blit(self.image, (0, 0))
 
 
 def update():
@@ -11,7 +50,7 @@ def update():
     map_params = {
         'll': ','.join(list(map(str, [x, y]))),
         'spn': ','.join(list(map(str, [delta_x, delta_y]))),
-        'l': 'map'
+        'l': l
     }
 
     map_response = requests.get(map_request, params=map_params)
@@ -28,21 +67,23 @@ def update():
         file.write(map_response.content)
 
 
-x, y = map(float, input('Координаты: ').split())
+y, x = map(float, input('Координаты: ').split())
 delta_x, delta_y = map(float, input('Масштаб: ').split())
-
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
 
 update()
 screen.blit(pygame.image.load('map.png'), (0, 0))
-
+bar = chouse_bar()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            map_r = bar.check(pygame.mouse.get_pos())
+            if map_r:
+                l = map_r
+            update()
+            screen.blit(pygame.image.load('map.png'), (0, 0))
         if event.type == pygame.KEYDOWN:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_PAGEUP]:
@@ -51,16 +92,23 @@ while running:
                 if delta_y > 100 or delta_x > 100:
                     delta_y = 0.001953125
                     delta_x = 0.001953125
-            if keys[pygame.K_PAGEDOWN]:
+            elif keys[pygame.K_PAGEDOWN]:
                 delta_x /= 2
                 delta_y /= 2
                 if delta_y < 0.001953125 or delta_x < 0.001953125:
                     delta_y = 90
                     delta_x = 90
-            print(delta_x, delta_y)
+            if keys[pygame.K_UP]:
+                y = y + 0.3 if y <= 84 else -85
+            elif keys[pygame.K_DOWN]:
+                y = y - 0.3 if y >= -84 else 85
+            elif keys[pygame.K_RIGHT]:
+                x = x + 0.3 if x <= 84 else -85
+            elif keys[pygame.K_LEFT]:
+                x = x - 0.3 if x >= -84 else 85
             update()
             screen.blit(pygame.image.load('map.png'), (0, 0))
-
+    bar.draw()
     pygame.display.flip()
 pygame.quit()
 os.remove('map.png')
